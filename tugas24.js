5caf67ecaa5d7f315162ebecdc4d4a0373862b00
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 16:52:56 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+var data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+
+function panggilSort (){
+	console.log(("Sebelumnya : "),(data.toString()))
+	console.log(("Ascending : " ),(data.sort().toString()))
+	console.log(("Descending : " ),(data.reverse().toString()))
+}
+
+panggilSort()
